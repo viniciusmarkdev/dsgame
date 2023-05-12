b8875b9323e8ce08adae5881236e7da307391580
Marcos Vinicius <contato.marcoscandido@gmail.com>
2023-05-12 20:53:35 -0300

    game seed - populando a tabela

--- src/main/java/com/projetcgame/dslist/entities/Game.java
+++ src/main/java/com/projetcgame/dslist/entities/Game.java
@@ -21,9 +21,12 @@ public class Game {
 		@Column(name="game_year")
 		private Integer year;
 		private String genre;
-		private String platform;
+		private String platforms;
+		private Double score;
 		private String imgUrl;
+		@Column(columnDefinition = "TEXT")
 		private String shortDescription;
+		@Column(columnDefinition = "TEXT")
 		private String longDescription;
 		
 		
@@ -33,7 +36,7 @@ public class Game {
 		}
 
 
-		public Game(Long id, String title, Integer year, String genre, String platform, String imgUrl,
+		public Game(Long id, String title, Integer year, String genre, String platforms, Double score ,String imgUrl,
 				String shortDescription, String longDescription) {
 			
 			this.id = id;
@@ -41,7 +44,8 @@ public class Game {
 	       
 			this.year = year;
 			this.genre = genre;
-			this.platform = platform;
+			this.platforms = platforms;
+			this.score = score;
 			this.imgUrl = imgUrl;
 			this.shortDescription = shortDescription;
 			this.longDescription = longDescription;
@@ -89,12 +93,23 @@ public class Game {
 
 
 		public String getPlatform() {
-			return platform;
+			return platforms;
 		}
 
 
-		public void setPlatform(String platform) {
-			this.platform = platform;
+		public void setPlatform(String platforms) {
+			this.platforms = platforms;
+		}
+
+		
+        
+		public Double getScore() {
+			return score;
+		}
+
+
+		public void setScore(Double score) {
+			this.score = score;
 		}
 
 
